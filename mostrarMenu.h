4d2433733a342dca9eca52dd6b7b20c6ee512fb7
Fernando270617 <107822824+Fernando270617@users.noreply.github.com>
2025-06-07 16:06:15 -0500

    mostrarMenu.h

---
+++ mostrarMenu.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Función para mostrar el menú
+void mostrarMenu() 
+{
+    printf("\n========== BIBLIOTECA ==========\n");
+    printf("1. Agregar libro\n");
+    printf("2. Ver todos los libros\n");
+    printf("3. Buscar libro\n");
+    printf("4. Cambiar estado (Disponible/Prestado)\n");
+    printf("5. Eliminar libro\n");
+    printf("6. Salir\n");
+    printf("Seleccione opción: ");
+}
